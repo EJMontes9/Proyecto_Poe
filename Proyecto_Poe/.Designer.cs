33f42344abcf1e063b9c68dfe596cfaba1977385
Memo-17 <guillermo_bame@outlook.com>
2021-07-16 20:26:59 -0500

    Cambio-nombre-form

--- Proyecto_Poe/.Designer.cs
+++ Proyecto_Poe/.Designer.cs
@@ -61,10 +61,10 @@ namespace Proyecto_Poe
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(175, 32);
+            this.label1.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 42);
+            this.label1.Size = new System.Drawing.Size(212, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "Iniciar Sesion";
             // 
