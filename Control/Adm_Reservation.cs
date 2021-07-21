6424b7cfe99d5e05de5191256ba99b6c8f4a9b71
EJMontes9 <60905959+EJMontes9@users.noreply.github.com>
2021-07-20 23:47:12 -0500

    Creacion de registro de reserva

---
+++ Control/Adm_Reservation.cs
@@ -0,0 +1,38 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    public class Adm_Reservation
+    {
+        List<Reservation> lisOrder = new List<Reservation>();
+        private static Reservation adm = null;
+        public static Reservation getAdm()
+        {
+            if (adm == null)
+            {
+                adm = new Reservation();
+            }
+            return adm;
+        }
+
+        public Reservation Get()
+        {
+            if (adm == null)
+            {
+                adm = new Reservation();
+            }
+            return adm;
+        }
+
+        public void Registro(string name, DateTime dateandTime, int people, string location, string suggestion)
+        {
+            Reservation obj = new Reservation(name, dateandTime, people, location, suggestion);
+            lisOrder.Add(obj);
+        }
+    }
+}
--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -9,9 +9,28 @@ namespace Model
     public class Reservation
     {
         private string name;
-        private DateTime DateandTime;
+        private DateTime dateandTime;
         private int people;
-        private string a;
+        private string location;
+        private string suggestion;
 
+        public Reservation(string name, DateTime dateandTime, int people, string location, string suggestion)
+        {
+            this.name = name;
+            this.dateandTime = dateandTime;
+            this.people = people;
+            this.location = location;
+            this.suggestion = suggestion;
+        }
+
+        public Reservation()
+        {
+        }
+
+        public string Name { get => name; set => name = value; }
+        public DateTime DateandTime { get => dateandTime; set => dateandTime = value; }
+        public int People { get => people; set => people = value; }
+        public string Location { get => location; set => location = value; }
+        public string Suggestion { get => suggestion; set => suggestion = value; }
     }
 }
