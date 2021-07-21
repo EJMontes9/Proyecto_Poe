9d134f1314c6ba457dcef18646598097ce18c2c5
Iván Ramos Ochoa <ivan.ramoso@ug.edu.ec>
2021-07-20 22:18:31 -0500

    HOla

---
+++ Proyecto_Poe/Frm_ClientRegister.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace Proyecto_Poe
+{
+    partial class Frm_ClientRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtCiudad = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.cmdRegistrar = new System.Windows.Forms.Button();
+            this.cmdRegresar = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Tag = "º";
+            this.label1.Text = "C.I:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Tag = "º";
+            this.label2.Text = "Nombres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Teléfono:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(403, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Correo electrónico:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(444, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ciudad:";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(159, 154);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(191, 20);
+            this.txtCedula.TabIndex = 6;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(159, 195);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(191, 20);
+            this.txtNombres.TabIndex = 7;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(159, 239);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(191, 20);
+            this.txtApellidos.TabIndex = 8;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(159, 286);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(191, 20);
+            this.txtTelefono.TabIndex = 9;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(519, 148);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(191, 20);
+            this.txtCorreo.TabIndex = 10;
+            // 
+            // txtCiudad
+            // 
+            this.txtCiudad.Location = new System.Drawing.Point(519, 195);
+            this.txtCiudad.Name = "txtCiudad";
+            this.txtCiudad.Size = new System.Drawing.Size(191, 20);
+            this.txtCiudad.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(438, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Dirección";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(519, 243);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(191, 20);
+            this.txtDireccion.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(438, 297);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Contraseña";
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(519, 290);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(191, 20);
+            this.txtContra.TabIndex = 15;
+            // 
+            // cmdRegistrar
+            // 
+            this.cmdRegistrar.Location = new System.Drawing.Point(224, 378);
+            this.cmdRegistrar.Name = "cmdRegistrar";
+            this.cmdRegistrar.Size = new System.Drawing.Size(104, 41);
+            this.cmdRegistrar.TabIndex = 16;
+            this.cmdRegistrar.Text = "Registrar";
+            this.cmdRegistrar.UseVisualStyleBackColor = true;
+            this.cmdRegistrar.Click += new System.EventHandler(this.cmdRegistrar_Click);
+            // 
+            // cmdRegresar
+            // 
+            this.cmdRegresar.Location = new System.Drawing.Point(390, 378);
+            this.cmdRegresar.Name = "cmdRegresar";
+            this.cmdRegresar.Size = new System.Drawing.Size(100, 41);
+            this.cmdRegresar.TabIndex = 17;
+            this.cmdRegresar.Text = "Regresar";
+            this.cmdRegresar.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(273, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(214, 26);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "REGISTRO DE USUARIO";
+            // 
+            // Frm_ClientRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 510);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cmdRegresar);
+            this.Controls.Add(this.cmdRegistrar);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCiudad);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_ClientRegister";
+            this.Tag = "º";
+            this.Text = "Contraseña";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtCiudad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.Button cmdRegistrar;
+        private System.Windows.Forms.Button cmdRegresar;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Proyecto_Poe/Frm_ClientRegister.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Poe
+{
+    public partial class Frm_ClientRegister : Form
+    {
+        public Frm_ClientRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdRegistrar_Click(object sender, EventArgs e)
+        {
+            String ci = txtCedula.Text;
+            String Nombre = txtNombres.Text;
+            String Apellido = txtApellidos.Text;
+            String Telefono = txtTelefono.Text;
+            String Correo = txtCorreo.Text;
+            String Ciudad = txtCiudad.Text;
+            String Direccion = txtDireccion.Text;
+            String Contraseña = txtContra.Text;
+
+        }
+    }
+}
